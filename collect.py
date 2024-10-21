d2806db80a2ffefa58ccd87b8f8438b88e60bb39
davidp <david.pop@tum.de>
2024-10-21 16:43:41 +0200

    added task description details to the saved data

--- collect.py
+++ collect.py
@@ -29,8 +29,8 @@ vehicle_data = []
 
 def save_video(frames, filename="carla_video.avi"):
     height, width, _ = frames[0].shape
-    print("视频分辨率：{}x{}".format(width, height))
-    print("帧数：{}".format(len(frames)))
+    print("resolution:{}x{}".format(width, height))
+    print("frame count: {}".format(len(frames)))
     video = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'XVID'), 20, (width, height))
 
     for frame in frames:
@@ -45,6 +45,21 @@ def save_vehicle_data(data, filename="vehicle_data.csv"):
                          "Throttle", "Brake", "Steer", "Gear", "Frame_Index"])
         for row in data:
             writer.writerow(row)
+            
+def save_combined_data(data, filename="vehicle_and_task_data.csv"):
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Timestamp", 
+                         "Location_X", "Location_Y", "Location_Z", 
+                         "Velocity_X", "Velocity_Y", "Velocity_Z",
+                         "Throttle", "Brake", "Steer", "Gear", 
+                         "Is_Junction", "Traffic_Light_Count",
+                         "Nearby_Vehicle_Count", "Nearby_Pedestrian_Count",
+                         "Cloudiness", "Precipitation", "Precipitation_Deposits",
+                         "Wind_Intensity", "Sun_Azimuth_Angle", "Sun_Altitude_Angle",
+                         "Frame_Index"])
+        for row in data:
+            writer.writerow(row)    
 
 def get_vehicle_data():
     transform = vehicle.get_transform()
@@ -57,6 +72,53 @@ def get_vehicle_data():
         velocity.x, velocity.y, velocity.z,
         control.throttle, control.brake, control.steer, control.gear
     ]
+    
+def generate_task_description():
+    weather = world.get_weather()
+    weather_data = [
+        weather.cloudiness,
+        weather.precipitation,
+        weather.precipitation_deposits,
+        weather.wind_intensity,
+        weather.sun_azimuth_angle,
+        weather.sun_altitude_angle
+    ]
+    
+    current_waypoint = world.get_map().get_waypoint(vehicle.get_location())
+    
+    # check for junction
+    is_junction = current_waypoint.is_junction
+    
+    # traffic lights
+    traffic_lights = world.get_actors().filter('traffic.traffic_light*')
+    relevant_lights = []
+    for tl in traffic_lights:
+        if tl.get_transform().location.distance(vehicle.get_location()) < 50:
+            relevant_lights.append(tl)
+    
+    # nearby vehicles
+    other_vehicles = world.get_actors().filter('vehicle.*')
+    nearby_vehicles = []
+    for ov in other_vehicles:
+        if ov.id != vehicle.id and ov.get_transform().location.distance(vehicle.get_location()) < 50:
+            nearby_vehicles.append(ov)
+            
+    # nearby pedestrians
+    pedestrians = world.get_actors().filter('walker.pedestrian.*')
+    nearby_pedestrians = []
+    for pedestrian in pedestrians:
+        if pedestrian.get_transform().location.distance(vehicle.get_location()) < 50:
+            nearby_pedestrians.append(pedestrian)
+    
+    # store task description
+    task_description = [
+        is_junction,
+        len(relevant_lights),
+        len(nearby_vehicles),
+        len(nearby_pedestrians),
+    ] + weather_data
+    
+    return task_description
 
 frame_count = 0
 def process_img(image):
@@ -66,10 +128,15 @@ def process_img(image):
     image_data = image_data[:, :, :3] 
     frames.append(image_data)
 
-    
+    # vechicle data
     vehicle_state = get_vehicle_data()
-    vehicle_state.append(frame_count) 
-    vehicle_data.append(vehicle_state)
+    
+    # task description data
+    task_description = generate_task_description()
+    
+    # combine data
+    combined_data = vehicle_state + task_description + [frame_count]
+    vehicle_data.append(combined_data)
 
     frame_count += 1
 
@@ -83,7 +150,8 @@ while time.time() - start_time < 10:
 
 camera.stop()
 save_video(frames)
-save_vehicle_data(vehicle_data)
+#save_vehicle_data(vehicle_data)
+save_combined_data(vehicle_data)
 
 vehicle.destroy()
 camera.destroy()
